58c9ee0fe05111ef9101ba6c3d32fb80053a665d
Songul <sngldlsz@gmail.com>
2022-04-14 11:50:28 +0100

    work3 is done about to be pushed to githup after commit

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -4,5 +4,7 @@ public class HelloWord {
     // doing some work here
     // doing awesome work2
 
-    //
+    //doing awesome work3
+    // awesome work 3 is done and ready to be commited
+    // and ready to be pushed remote repository
 }
